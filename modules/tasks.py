32e0835b3ce3c0b09c472d04644bd1d64684a60c
Mezentsev Andrey <mezentsew@mail.ru>
2024-08-08 12:32:03 +0300

    Updated modules: tasks.py, tests.py.

--- modules/tasks.py
+++ modules/tasks.py
@@ -16,7 +16,8 @@ def send_updates(item: typing.Any) -> typing.Any:
         for item in active_subscriptions:
             send_mail(
                 subject=f"Обновление модуля: '{item.module.title}'",
-                message=f"Информируем, что обновлен модуль: '{item.module.title}'",
+                message=f"Информируем, что обновлен модуль: '{item.module.title}'\n"
+                        f"\n* Данное сообщение является автоматическим уведомлением и не требует ответа.",
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=[item.subscriber.email],
                 fail_silently=False,
